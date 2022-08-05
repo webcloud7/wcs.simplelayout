8b145c37cbbbb7b65fea8a325152ecc9532908fc
Mathias Leimgruber <m.leimgruber@webcloud7.ch>
2022-08-04 20:34:00 -0400

    Update slblocks_layout uids after copy/paste.

---
+++ wcs/simplelayout/subscribers.py
@@ -0,0 +1,31 @@
+from Acquisition import aq_parent
+from plone.uuid.interfaces import IUUID
+from wcs.simplelayout.contenttypes.behaviors import ISimplelayout
+import json
+
+
+def update_page_state_on_copy_paste_block(block, event):
+    """Update the uid of the new created block in the page state.
+    block: new block
+    event.original: origin of the copy event - usually the simplelayout page"""
+    parent = aq_parent(block)
+    original_parent = event.original
+
+    if not parent:
+        return
+
+    if len(parent.getPhysicalPath()) > 1:
+        # If we are in a child page of the copied page get the actual parent
+        original_parent = event.original.restrictedTraverse(
+            '/'.join(parent.getPhysicalPath()[1:]))
+
+    # Only update page state, if the original object is a Simplelayout page.
+    if not ISimplelayout.providedBy(original_parent):
+        return
+
+    origin_block_uid = IUUID(original_parent.get(block.id))
+    page_layout = ISimplelayout(parent).slblocks_layout
+
+    new_block_uid = IUUID(block)
+    ISimplelayout(parent).slblocks_layout = json.loads(
+        json.dumps(page_layout).replace(origin_block_uid, new_block_uid))
