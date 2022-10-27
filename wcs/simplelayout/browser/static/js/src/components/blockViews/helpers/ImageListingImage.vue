c0bc7dd3e3ecaacc38a109e444df43d0e96f0c52
Mathias Leimgruber <m.leimgruber@webcloud7.ch>
2022-10-27 18:06:20 -0400

    Fix image listing, if there is no image.

---
+++ wcs/simplelayout/browser/static/js/src/components/blockViews/helpers/ImageListingImage.vue
@@ -0,0 +1,34 @@
+<template>
+  <figure class="d-table m-0 text-center">
+    <img
+      :src="image.image.scales.preview.download"
+      v-if="image.image && image.image.scales && image.image.scales.preview"
+    />
+    <template v-else-if="image.file">
+      <img
+        :src="`${sl.baseURL}/@@iconresolver/mimetype-${image.file['content-type']}`"
+      />
+    </template>
+    <template v-else>
+      <img :src="`${sl.baseURL}/@@iconresolver/x-octagon`" />
+    </template>
+    <figcaption v-if="image.title" class="figure-caption mt-1">
+      {{ image.title }}
+    </figcaption>
+  </figure>
+</template>
+<script>
+import { useSimplelayoutStore } from "@/store.js";
+export default {
+  props: {
+    image: {
+      type: Object,
+      required: true,
+    },
+  },
+  setup() {
+    const sl = useSimplelayoutStore();
+    return { sl };
+  },
+};
+</script>
