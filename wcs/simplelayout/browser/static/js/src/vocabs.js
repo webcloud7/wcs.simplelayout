849f1ba384374c54a3cc28b28ceda98f30e23c83
Mathias Leimgruber <m.leimgruber@webcloud7.ch>
2024-05-31 11:41:41 -0400

    Add 66% to width options.

--- wcs/simplelayout/browser/static/js/src/vocabs.js
+++ wcs/simplelayout/browser/static/js/src/vocabs.js
@@ -2,6 +2,7 @@ function ColWidths(asMapping) {
   const widths = [
     { cols: 12, label: "100%" },
     { cols: 9, label: "75%" },
+    { cols: 8, label: "66%" },
     { cols: 6, label: "50%" },
     { cols: 4, label: "33%" },
     { cols: 3, label: "25%" },
